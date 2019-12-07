423c2c6db2cd3ed83b9cec878498e1008520f2eb
Devjyupeter <56060158+Devjyupeter@users.noreply.github.com>
2019-12-07 06:57:56 -0600

    Update my_project.ino

--- my_project.ino
+++ my_project.ino
@@ -1,56 +1,54 @@
 #include <LiquidCrystal.h>
 #include <Servo.h>
-LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
+LiquidCrystal lcd(7, 6, 5, 4, 3, 2); // set the digital pins to display on LCD
 
 
-Servo myservo;  // create servo object to control a servo 
-                // a maximum of eight servo objects can be created 
- 
-int pos = 0;    // variable to store the servo position 
- 
-//int ledPin = 10;
-int sensorPin = A0;
-int value;
+Servo myservo;  // create servo object to control a servo
+// a maximum of eight servo objects can be created
+
+int pos = 0;    // variable to store the servo position
+
+
+int sensorPin = A0; // set the alcohol sensor pin
+int value;    //variable to store the sensor value
 
 void setup()
 {
-Serial.begin(9600);
-lcd.begin(16,2);
-//pinMode(ledPin,OUTPUT);
-myservo.attach(10);  // attaches the servo on pin 9 to the servo object
+  Serial.begin(9600); // set the baud rate
+  lcd.begin(16, 2); // set the cursor of the LCD
+  myservo.attach(10);  // attaches the servo on pin 10 to the servo object
 }
-void loop() 
+void loop()
 {
-int Value = analogRead(sensorPin);
-value = analogRead(A0);
-lcd.print("Alcohol Lev.:");
-lcd.print(value-50);
-Serial.print(value);
-if (value-50 > 400) 
+  int Value = analogRead(sensorPin);
+  value = analogRead(A0);
+  lcd.print("Alcohol Lev.:");
+  lcd.print(value - 50);
+  Serial.print(value);
+  if (value - 50 > 400) // condition to display alert
 
-{
+  {
     //digitalWrite(ledPin,HIGH);
-     lcd.setCursor(0, 2);
-     lcd.print("Alert....!!!");
+    lcd.setCursor(0, 2);
+    lcd.print("Alert....!!!");// printing Alert...!!! to the lcd
     Serial.print ("Alert");
-   for(pos = 0; pos < 180; pos += 1)  // goes from 0 degrees to 180 degrees 
-  {                                  // in steps of 1 degree 
-    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
-  //  delay(15);                       // waits 15ms for the servo to reach the position 
-  }   
-  } 
+    for (pos = 0; pos < 180; pos += 1) // goes from 0 degrees to 180 degrees
+    { // in steps of 1 degree
+      myservo.write(pos);              // tell servo to go to position in variable 'pos'
+      //  delay(15);                       // waits 15ms for the servo to reach the position
+    }
+  }
   else {
-      //digitalWrite(ledPin,LOW); 
-      lcd.setCursor(0, 2);
-      lcd.print(".....Normal.....");
-      Serial.print("Normal");
-    for(pos = 180; pos>=1; pos-=1)     // goes from 180 degrees to 0 degrees 
-      {                                
-        myservo.write(pos);              // tell servo to go to position in variable 'pos' 
-       // delay(15);                       // waits 15ms for the servo to reach the position 
-      } 
-      }
+    lcd.setCursor(0, 2);
+    lcd.print(".....Normal.....");
+    Serial.print("Normal");
+    for (pos = 180; pos >= 1; pos -= 1) // goes from 180 degrees to 0 degrees
+    {
+      myservo.write(pos);              // tell servo to go to position in variable 'pos'
 
-delay(500);
-lcd.clear();
+    }
+  }
+
+  delay(500);
+  lcd.clear();
 }
